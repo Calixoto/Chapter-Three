78c27c7a6370d4343e9286e9fbc1d69ce09f9b55
Calixoto <grimmjow006@live.com>
2021-06-01 17:56:06 -0300

    adicionando typescript e estilização SASS

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import style from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <h1
+      className={style.title}
+    >
+      hello <span>world</span>
+    </h1>
+  )
+}
