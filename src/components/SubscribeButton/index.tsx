5965b1017a888a624a35634b895c01bca5e63269
calixoto <grimmjow006@live.com>
2021-07-09 12:07:16 -0300

    configurando pagamentos no stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +8,34 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession();
+
+    async function handleSubscribeButton() {
+        if (!session) {
+            signIn('github');
+            return;
+        }
+
+        try {
+            const response = await api.post('/subscribe');
+
+            const { sessionId } = response.data;
+
+            const stripe = getStripeJs();
+
+            (await stripe).redirectToCheckout({ sessionId });
+        } catch (err) {
+            alert(err.message);
+
+        }
+
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribeButton}
         >
             Subscribe now
         </button>
